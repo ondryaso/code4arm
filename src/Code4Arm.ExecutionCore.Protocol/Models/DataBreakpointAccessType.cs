bf4a0931fafb920f3c4cd10463772f066801abd8
Ondřej Ondryáš <ondryaso@ondryaso.eu>
2022-07-06 20:27:21 +0200

    Update licensing texts

--- src/Code4Arm.ExecutionCore.Protocol/Models/DataBreakpointAccessType.cs
+++ src/Code4Arm.ExecutionCore.Protocol/Models/DataBreakpointAccessType.cs
@@ -2,7 +2,8 @@
 // Original source: https://github.com/OmniSharp/csharp-language-server-protocol
 // Original author: David Driscoll (and contributors)
 // Modified by: Ondřej Ondryáš
-// Licensed under the MIT License. Copyright (c) Ondřej Ondryáš, .NET Foundation and Contributors.
+// Licensed under the MIT License, licensing details are available in README.md.
+// Copyright (c) Ondřej Ondryáš, .NET Foundation and Contributors.
 
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
